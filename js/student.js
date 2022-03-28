e28780c50b802e59a876f8fac8e009d6b918b855
AnhM1nh <anhminh23031999@gmail.com>
2022-03-28 22:51:23 +0700

    up

--- js/student.js
+++ js/student.js
@@ -3,12 +3,10 @@ function save() {
     let age = document.getElementById('age').value;
     let place = document.getElementById('place').value;
     let point = document.getElementById('point').value;
-    let gender = '';
-        if (document.getElementById('male').checked) {
-            gender = document.getElementById('male').value;
-        } else if ((document.getElementById('famale').checked)) 
-            gender = document.getElementById('famale').value;
+    let gender = document.getElementById('gender').value;
     
+    
+    checkInput();
     let students = localStorage.getItem('students') ? JSON.parse(localStorage.getItem('students')) : [];
     
     students.push({
@@ -17,12 +15,77 @@ function save() {
         place: place,
         point: point,
         gender: gender,
-    });
-
-   localStorage.setItem('students', JSON.stringify(students));
-
-   this.renderListStudent();
+    }
+    );
+    
+    localStorage.setItem('students', JSON.stringify(students));
+    
+    this.renderListStudent();
 }
+
+function checkInput() {
+    const fullnameValue = fullname.value;
+    const ageValue = age.value;
+    const placeValue = place.value;
+    const pointValue = point.value;
+    const genderValue = gender.value;
+
+    if(fullnameValue === '') {
+        setErrorFor(fullname, 'Vui lòng nhập họ và tên');
+    } else {
+        setSuccess(fullname);
+    }
+    
+    if(isNaN(ageValue)) {
+        setErrorFor(age, 'Tuổi không hợp lệ');
+    } if (ageValue === '') {
+        setErrorFor(age, 'Tuổi không hợp lệ');
+    } else {
+        setSuccess(age);
+    }
+
+    if(placeValue === '') {
+        setErrorFor(place, 'Vui lòng nhập quê quán');
+    } else {
+        setSuccess(place);
+    }
+
+    if(pointValue === '' || isNaN(pointValue) > 10) {
+        setErrorFor(point, 'Điểm không hợp lệ');
+    } else {
+        setSuccess(point);
+    }
+
+    if (genderValue !== 'Nam' && genderValue !== 'Nữ') {
+        setErrorFor(gender, 'Giới tính không hợp lệ');
+    } else {
+        setSuccess(gender);
+    }
+    
+    if (fullnameValue === '' || isNaN(ageValue) || ageValue === '' || placeValue === '' || 
+        isNaN(pointValue) || isNaN(pointValue) > 10 || pointValue === '' || genderValue !== 'Nam' && genderValue !== 'Nữ') {
+        preventDefault()    
+    } 
+
+}
+
+const setErrorFor = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    // inputControl.classList.remove('error');
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    // inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
 
 function renderListStudent() {
     let students = localStorage.getItem('students') ? JSON.parse(localStorage.getItem('students')) : [];
@@ -39,13 +102,12 @@ function renderListStudent() {
     </tr>`;
     
     students.forEach((student, index) => {
-    let renderLabel = parseInt(student.gender) === 1 ? 'Nam' : 'Nữ';
     table += `<tr>
         <td>${student.fullname}</td>
         <td>${student.age}</td>
         <td>${student.place}</td>
         <td>${student.point}</td>
-        <td>${renderLabel}</td>
+        <td>${student.gender}</td>
         <td>
             <button onclick = "editItem(${index})" class="edit">Edit</button> |
             <button onclick = "deleteItem(${index})">Delete</button>
@@ -70,6 +132,8 @@ function editItem(id) {
     age.value = students[id].age
     place.value = students[id].place
     point.value = students[id].point
+    gender.value = students[id].gender
+
     document.getElementById("index").value = id
 
     document.getElementById("update").style.display="inline-block"
@@ -78,12 +142,12 @@ function editItem(id) {
 function update() {
     let students = localStorage.getItem('students') ? JSON.parse(localStorage.getItem('students')) : [];
     let id = document.getElementById("index").value
-    console.log(id)
     students[id] = {
         fullname: document.getElementById('fullname').value,
         age: document.getElementById('age').value,
         place: document.getElementById('place').value,
         point: document.getElementById('point').value,
+        gender: document.getElementById('gender').value,
     }
     localStorage.setItem('students', JSON.stringify(students));
     this.renderListStudent()
